76b8de1f80b9367d55163b739a1ec82cf79330f2
Aleksey Kulikov <skinny.mind@gmail.com>
2022-04-21 15:16:32 +0300

    test(diff): fix test failing on Windows CI

--- test/diff_test.dart
+++ test/diff_test.dart
@@ -128,7 +128,9 @@ index e69de29..c217c63 100644
 
   tearDown(() {
     repo.free();
-    tmpDir.deleteSync(recursive: true);
+    if (Platform.isLinux || Platform.isMacOS) {
+      tmpDir.deleteSync(recursive: true);
+    }
   });
 
   group('Diff', () {
